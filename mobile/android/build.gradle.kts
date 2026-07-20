f883d4ce7d97bdadb620d02a21ad5e164dd14373
Julie <julie@epita.fr>
2026-07-20 18:15:29 +0200

    fix: force all plugins to compileSdk 36

---
+++ mobile/android/build.gradle.kts
@@ -0,0 +1,32 @@
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val ext = project.extensions.findByName("android")
+            if (ext is com.android.build.gradle.BaseExtension) {
+                ext.compileSdkVersion(36)
+            }
+        }
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
